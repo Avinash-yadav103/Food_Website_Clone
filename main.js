5aab238341705a4a9e8cb8a038014e6b9fc7ed00
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-04-21 20:40:53 +0530

    commit 1

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.querySelector('.participate').addEventListener('click', function() {
+//     document.querySelector('.participate').classList.toggle('participate_border_active');
+// });
+
+function addClass() {
+    let element = document.getElementById("myDiv");
+    element.classList.add("highlight");
+    }
